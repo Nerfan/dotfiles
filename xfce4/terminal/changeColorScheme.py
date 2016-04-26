e71581944081a96c13a2b49b3df7133bb7a08053
jeremy <jerelefurge@gmail.com>
2016-04-25 20:56:13 -0400

    Moved fading setting from i3 config to compton.conf

--- xfce4/terminal/changeColorScheme.py
+++ xfce4/terminal/changeColorScheme.py
@@ -3,44 +3,73 @@ Provides an easy way to change terminalrc files for xfce4-terminal.
 
 I made this so that all of my other settings (really just the font) wouldn't change
 every time I wanted to change color scheme.
-TODO: add support for foreground and background
-add support for all color options
+Every input and output file is an xfce4-terminalrc
+
+TODO: add support for all color options
 """
 
 import sys
 
-def setColors(newColors, terminalrcLocation="/home/jeremy/.config/xfce4/terminal/terminalrc"):
-     """
-     Takes a string of color odes and replaces the colors currently set in terminalrc.
- 
-     Can also take an argument for where the terminalrc is
-     
-     newColors: String of 12-digit color codes; 16 different colors to be put in
-     terminalrcLocation: Full path to the terminalrc file to be edited; unneeded argument
-     """
-     terminalrc = open(terminalrcLocation)
-     filebefore = ""
-     for line in terminalrc:
-         if line[0:13] == "ColorPalette=":
-             filebefore += line[0:13] + newColors + "\n"
-         else:
-             filebefore += line
-     terminalrc.close()
-     terminalrc = open(terminalrcLocation, "w")
-     terminalrc.truncate()
-     terminalrc.write(filebefore)
-     terminalrc.close()
+terminalrcLocation="/home/jeremy/.config/xfce4/terminal/terminalrc"
 
-def saveColors(themeName, sourceFile="/home/jeremy/.config/xfce4/terminal/terminalrc"):
+def getForeground(themeFile):
     """
-    Saves the current color configuration to a file named by the parameter
+    Returns the foreground color from a theme file
+    """
+    for line in open(themeFile):
+        if line[0:16] == "ColorForeground=":
+            return line[16:]
 
-    themeName: String, what to call the theme
-    sourceFile: String, full path to the file to draw the colors from (defaults to terminalrc)
+
+def setForeground(fgColor):
     """
-    configFile = open(themeName, "w")
-    colors = getColors(sourceFile)
-    configFile.write("ColorPalette=" + colors)
+    Takes a single color code for the foreground (text color) and applies it to a terminalrc file
+
+    fgColor: String, 12-digit color code for the text color in the terminal
+    """
+    terminalrc = open(terminalrcLocation)
+    filebefore = ""
+    for line in terminalrc:
+        if line[0:16] == "ColorForeground=":
+            filebefore += line[0:16] + fgColor
+        else:
+            filebefore += line
+    terminalrc.close()
+    terminalrc = open(terminalrcLocation, "w")
+    terminalrc.truncate()
+    terminalrc.write(filebefore)
+    terminalrc.close()
+    print("Foreground changed")
+
+
+def getBackground(themeFile):
+    """
+    Returns the background color from a theme file
+    """
+    for line in open(themeFile):
+        if line[0:16] == "ColorBackground=":
+            return line[16:]
+
+
+def setBackground(bgColor):
+    """
+    Takes a single color code for the foreground (text color) and applies it to a terminalrc file
+
+    fgColor: String, 12-digit color code for the text color in the terminal
+    """
+    terminalrc = open(terminalrcLocation)
+    filebefore = ""
+    for line in terminalrc:
+        if line[0:16] == "ColorBackground=":
+            filebefore += line[0:16] + bgColor
+        else:
+            filebefore += line
+    terminalrc.close
+    terminalrc = open(terminalrcLocation, "w")
+    terminalrc.truncate()
+    terminalrc.write(filebefore)
+    terminalrc.close()
+    print("Background changed")
 
 
 def getColors(configFileName):
@@ -51,18 +80,65 @@ def getColors(configFileName):
     """
     config = open(configFileName)
     for line in config:
-        if line[0:13] == "ColorPalette=":
-            return line[13:]
+        if line[0:13] == "ColorPalette=": # Includes the ending newline character
+           return line[13:]
+        
 
+def setColors(newColors):
+    """
+    Takes a string of color odes and replaces the colors currently set in terminalrc.
+    
+    newColors: String of 12-digit color codes; 16 different colors to be put in
+    terminalrcLocation: Full path to the terminalrc file to be edited; unneeded argument
+    """
+    terminalrc = open(terminalrcLocation)
+    filebefore = ""
+    for line in terminalrc:
+        if line[0:13] == "ColorPalette=":
+            filebefore += line[0:13] + newColors
+        else:
+            filebefore += line
+    terminalrc.close()
+    terminalrc = open(terminalrcLocation, "w")
+    terminalrc.truncate()
+    terminalrc.write(filebefore)
+    terminalrc.close()
+
+            
+def saveColors(themeName, sourceFile=terminalrcLocation):
+    """
+    Saves the current color configuration to a file named by the parameter
+
+    Includes colorscheme as well as foreground and background
+
+    themeName: String, what to call the theme
+    sourceFile: String, full path to the file to draw the colors from (defaults to terminalrc)
+    """
+    configFile = open(themeName, "w")
+    colors = getColors(sourceFile)
+    configFile.write("ColorPalette=" + colors)
+    configFile.write("ColorForeground=" + getForeground(sourceFile))
+    configFile.write("ColorBackground=" + getBackground(sourceFile))
+
+
+# Main function
 if __name__=="__main__":
-    if len(sys.argv) == 2:
-        setColors(getColors(sys.argv[1]))
-    elif len(sys.argv) == 3:
-        if sys.argv[1] == "save":
-            saveColors(sys.argv[2])
-        if sys.argv[1] == "load":
-            setColors(getColors(sys.argv[2]))
-    else:
+    if len(sys.argv) < 2:
         print("Usage: python3.5 changeColorScheme [save|load] themeFileName")
         exit(0)
+    elif len(sys.argv) == 2:
+        setColors(getColors(sys.argv[1]))
+    elif len(sys.argv) >= 3:
+        config = sys.argv[2] # The second argument will always be the name of the config
+        if sys.argv[1] == "save": # If we're just saving
+            saveColors(config)
+        elif sys.argv[1] == "load": # Or if we're loading an existing config
+            setColors(getColors(config)) # Always set the colors
+            for i in range(3, len(sys.argv)): # Check for additional arguments (i.e. fg and bg)
+                if sys.argv[i] == "fg":
+                    setForeground(getForeground(config))
+                elif sys.argv[i] == "bg":
+                    setBackground(getBackground(config))
 
+
+# End of file
