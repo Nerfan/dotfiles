448db9b16facf382287e870af98a182307f6aeed
jeremy <jerelefurge@gmail.com>
2020-01-24 14:26:24 -0500

    Fixed weather script

--- polybar/weather.sh
+++ polybar/weather.sh
@@ -8,5 +8,5 @@ URL='https://darksky.net/forecast/43.1548,-77.6192/us12/en'
 wget -q -O- "$URL" | \
 awk -F'[;>˚]' '
 /summary swap/ {printf("%d° ", $2)}
-/Feels Like/ {printf("%d°\n", $5)}
+/feels-like-text/ {printf("%s°\n", $6)}
 '
